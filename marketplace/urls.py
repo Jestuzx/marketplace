d52d5e13f77d53198baf5a616ea890ddb135fc2e
Kogarashi <ycheba38@gmail.com>
2025-11-28 11:37:33 +0100

    Set home page as product catalog

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-
+from products.views import ProductListView
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", ProductListView.as_view(), name="home"),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("products/", include("products.urls", namespace="products")),
